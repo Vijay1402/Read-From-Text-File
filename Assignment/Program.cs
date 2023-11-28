ebe62642629689fb901b4e92393963523c6f3f02
Vijay1402 <rvrblr@gmail.com>
2023-11-28 18:24:39 +0530

    Assignment

---
+++ Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static readonly string textFile = @"StudentDetails.txt";
+        static void Main(string[] args)
+        {
+            if (File.Exists(textFile))
+            {
+                Console.WriteLine("Here are the Student Details");
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(ln);
+                    }
+                    file.Close();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
